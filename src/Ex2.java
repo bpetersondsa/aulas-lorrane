87e569ec84313ded6c4ed2cc4ea27e3a09d61f11
Bruno <bruno@Brunos-MacBook-Air.local>
2026-03-28 22:10:54 -0300

    .

---
+++ src/Ex2.java
@@ -0,0 +1,14 @@
+public class Ex2 {
+    public static void main(String[] args) {
+
+        double nota = 7;
+
+        if (nota >= 7) {
+            System.out.println("Aprovado");
+        } else if (nota >= 5) {
+            System.out.println("Recuperacao");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
